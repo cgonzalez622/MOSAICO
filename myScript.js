00f9b809491a6cfa58e9ae352f83395c9791fd46
Camille <c.gonzalez0017@gmail.com>
2025-07-25 16:56:54 -0500

    commit

--- myScript.js
+++ myScript.js
@@ -7,3 +7,63 @@ for (i = 0; i < toggler.length; i++) {
     this.classList.toggle("caret-down");
   });
 }
+
+var toggler = document.getElementsByClassName("caret1");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret1-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("caret2");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret2-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("caret3");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret3-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("caret4");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret4-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("caret5");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret5-down");
+  });
+}
+
+var toggler = document.getElementsByClassName("caret6");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret6-down");
+  });
+}
